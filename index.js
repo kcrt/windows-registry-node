ecd885a42ebf013ee3217bb75e6d4ee45936b7e1
TAKAHASHI Kyohei <kcrt@kcrt.net>
2017-09-22 19:19:54 +0900

    Add check for Windows platform
    Now we can safely import/require this library without crashing under
    non-Windows environment.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-module.exports = {
-    registry: require('./lib/registry'),
-    error: require('./lib/error'),
-    types: require('./lib/types'),
-    windef: require('./lib/windef'),
-    Key: require('./lib/key'),
-    utils: require('./lib/utils')
-};
+if (process.platform == "win32") {
+	module.exports = {
+		registry: require('./lib/registry'),
+		error: require('./lib/error'),
+		types: require('./lib/types'),
+		windef: require('./lib/windef'),
+		Key: require('./lib/key'),
+		utils: require('./lib/utils')
+	};
+} else {
+	module.exports = void 0;
+}
